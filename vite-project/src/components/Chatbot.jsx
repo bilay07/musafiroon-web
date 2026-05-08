70b17d44b498e6e91c6cf2ca950b391206e66d30
Muhammad Bilal <2022s-mulbscs-003@mul.edu.pk>
2026-05-08 21:40:51 +0500

    new

--- vite-project/src/components/Chatbot.jsx
+++ vite-project/src/components/Chatbot.jsx
@@ -68,7 +68,7 @@ function Chatbot() {
         method: "POST",
         headers: {
           "Authorization": voiceflowApiKey,
-          "versionID": "V1.04", 
+          "versionID": "production", 
           "Content-Type": "application/json",
           "accept": "application/json"
         },
