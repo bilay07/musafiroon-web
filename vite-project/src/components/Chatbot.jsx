bcb37945ff8d7fcee39483d838ce489b19adce91
Muhammad Bilal <2022s-mulbscs-003@mul.edu.pk>
2026-05-11 14:02:21 +0500

    update

--- vite-project/src/components/Chatbot.jsx
+++ vite-project/src/components/Chatbot.jsx
@@ -59,7 +59,7 @@ function Chatbot() {
     setIsTyping(true);
 
     try {
-   const response = await fetch(`https://musafiroon-web.onrender.com/api/chat`, {
+      const response = await fetch(`http://localhost:5000/api/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 
